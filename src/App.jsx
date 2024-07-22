a1c0b78690788447bff738ba45baa9e8aa05e28b
wasupon <wasupon.mail@gmail.com>
2024-07-22 10:02:15 +0900

    fix: reset timer for each question

--- src/App.jsx
+++ src/App.jsx
@@ -386,7 +386,7 @@ function App() {
     setScore(0);
     setIsNewGame(false);
     setIsActive(true);
-    setTimeLeft(formData.trivia_number_question * 5); // Reset timer when start game
+    setTimeLeft(formData.trivia_number_question * 20); // Reset timer when start game
   };
 
   const endGame = (timeLeft) => {
